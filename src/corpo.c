3b0ab0487232c7bcbd8a2e9eec0bb8f53a0d48d9
Yan Lucena <47032404+ylucena40@users.noreply.github.com>
2019-12-14 06:10:14 -0300

    Add files via upload

--- src/corpo.c
+++ src/corpo.c
@@ -1,7 +1,7 @@
 #include "corpo.h"
 #include "math.h"
 
-void desenha_corpo(){
+void desenha_corpo(float cauday){
 
     glPushMatrix();
         glRotatef(180, 0, 1, 0);
@@ -24,8 +24,61 @@ void desenha_corpo(){
         
         gluCylinder(gluNewQuadric(), 15, 15, 30, 32, 32);
         glTranslatef(0, 0, 30);
+
+        gluCylinder(gluNewQuadric(), 15, 5, 20, 32, 32);
+        glTranslatef(0, 0, 20);
+		gluCylinder(gluNewQuadric(), 5, 1, 1, 32, 32);
+
+		//glColor3f(0.0f, 0.0f, 1.0f);
+		glRotatef(25, 1.0, 0.0, 0.0);
+		glTranslatef(0.0,4.0,0.0);
+		glScalef(0.5f, 5.0f, 2.0f);
+		glutSolidCube(4.0f);
+		glScalef(5.0f, 0.25, 0.5f);
+		
+
+
+		glRotatef(-20, 1.0, 0.0, 0.0);
+		glTranslatef(0.0,7.0,4.1);
+		glScalef(0.5,1.0,2.6);
+		glutSolidCube(4.0f);
+		glScalef(2.0,1.0,0.38);
+		glTranslatef(0.0,-7.0,-4.1);
+		glRotatef(20, 1.0, 0.0, 0.0);
+
+
+		glTranslatef(0.0,0.0,5.0);
+		
+		
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glTranslatef(0.0,6.0,0.0);
+		glScalef(0.5,1.0,1.0);
+		glutSolidCube(4.0f);
+		glScalef(2.0,1.0,1.0);
+		glTranslatef(0.0,-6.0,0.0);
+		glTranslatef(0.0,-6.0,0.0);
+		glScalef(0.5,1.0,1.0);
+		glutSolidCube(4.0f);
+		glScalef(2.0,1.0,1.0);
+		glTranslatef(0.0, 6, 0.0);
+
+		
+
+		glTranslatef(0.0,0.0,-2.0);
+		glRotatef(cauday, 0.0,1.0,0.0);
+		glScalef(0.1,0.1,0.1);
+		glutSolidSphere(1, 10 ,10);
+		glScalef(10,10,10);
+		glTranslatef(0.0,0.0,2.0);
+		glColor3f(1.0,0.0,0.0);
+		glScalef(0.5f, 2.0f, 1.0f);
+		glutSolidCube(4.0f);	
+		glScalef(2.0f,0.5f,1.0f);
+		glRotatef(-cauday, 0.0,1.0,0.0);
+
+		glRotatef(-25, 1.0, 0.0, 0.0);
         
-        gluDisk(gluNewQuadric(), 0, 15, 32, 32);
+        //gluDisk(gluNewQuadric(), 0, 15, 32, 32);
     glPopMatrix();
 
 }
