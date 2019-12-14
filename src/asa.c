60040542fb5462d5ea300abf7e2774b113fef8f0
Yan Lucena <47032404+ylucena40@users.noreply.github.com>
2019-12-14 13:13:49 -0300

    Add files via upload

--- src/asa.c
+++ src/asa.c
@@ -5,6 +5,7 @@ void desenha_asa(float asax){
 
     glPushMatrix();
 	glColor3f(1.0f, 1.0f, 1.0f);
+		//asa direita
 		//asa de baixo
 
 		glScalef(2.0,1.0,1.0);
@@ -124,6 +125,134 @@ void desenha_asa(float asax){
 		glutSolidCube(1.0f);
 		glScalef(0.1,1.0,0.066);
 
+		glTranslatef(-17.75,0.0,0.0);
+		
+		glTranslatef(-17.75,0.0,0.0);
+		glTranslatef(17.75,0.0,0.0);
+
+
+		glRotatef(-180,1,0,0);
+
+		glTranslatef(-50,0.0,0.0);
+		glTranslatef(0,0.0,15.0);
+
+		//asa esquerda11
+
+		//asa de baixo
+		glColor3f(1.0,1.0,1.0);
+		glRotatef(180,0.0,1.0,0.0);
+
+		glTranslatef(0.0,-20.0,0.0);
+
+		glScalef(45.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.022,1.0,0.066);
+
+		glTranslatef(22,0.0,7.5);
+		glScalef(7.5f,0.833,15.0f);
+		glutSolidSphere(1, 10 ,10);
+		glScalef(0.133,1.2,0.066);
+
+		glTranslatef(-22,0.0,-7.5);
+		glTranslatef(0.0,0.0,15.0);
+		glTranslatef(-17.75,0.0,0.0);
+		glScalef(10.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.1,1.0,0.066);
+
+		glTranslatef(17.75,0.0,0.0);
+		glTranslatef(17.75,0.0,0.0);
+
+		glColor3f(1.0,1.0,1.0);
+		glScalef(10,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.1,1.0,0.066);
+
+
+		glTranslatef(-17.75,0.0,0.0);
+		glTranslatef(0.0,0.0,-7.5);
+		glScalef(13.5,0.1,0.1f);
+		glutSolidSphere(1, 10 ,10);
+		glScalef(0.074,10.0,10.0);
+		
+
+		glTranslatef(0.0,0.0,7.5);
+		glScalef(27.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.037,1.0,0.066);
+
+		glTranslatef(-17.75,0.0,0.0);
+		glTranslatef(17.75,0.0,0.0);
+		glTranslatef(0.0,0.0,-15.0);
+
+		//astes
+
+		//glTranslatef(20,0,0);
+		glTranslatef(0.0,10.0,0.0);
+
+
+		glTranslatef(15,0.0,0.0);
+		glScalef(1.0,20.0,1.0);
+		glutSolidCube(1.0f);
+		glScalef(1.0,0.05,1.0);
+		glTranslatef(-15,0.0,0.0);
+
+		glTranslatef(0.0,0.0,15.0);
+		glTranslatef(15,0.0,0.0);
+		glScalef(1.0,20.0,1.0);
+		glutSolidCube(1.0f);
+		glScalef(1.0,0.05,1.0);
+		glTranslatef(-15,0.0,0.0);
+		glTranslatef(0.0,0.0,-15.0);
+
+		glTranslatef(-15,0.0,0.0);
+		glScalef(1.0,20.0,1.0);
+		glutSolidCube(1.0f);
+		glScalef(1.0,0.05,1.0);
+		glTranslatef(15,0.0,0.0);
+
+		glTranslatef(0.0,0.0,15.0);
+		glTranslatef(-15,0.0,0.0);
+		glScalef(1.0,20.0,1.0);
+		glutSolidCube(1.0f);
+		glScalef(1.0,0.05,1.0);
+		glTranslatef(15,0.0,0.0);
+		glTranslatef(0.0,0.0,-15.0);
+
+
+		glTranslatef(0.0,-10.0,0.0);
+		//glTranslatef(-20.0,0.0,0.0);
+
+		//asa de cima
+
+		glColor3f(1.0,1.0,1.0);
+
+		glTranslatef(0.0,20.0,0.0);
+
+		glScalef(45.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.022,1.0,0.066);
+
+		glTranslatef(22,0.0,07.5);
+		glScalef(7.5f,0.833,15.0f);
+		glutSolidSphere(1, 10 ,10);
+		glScalef(0.133,1.2,0.066);
+
+		glTranslatef(-22,0.0,-7.5);
+		glTranslatef(0.0,0.0,15.0);
+		glTranslatef(-17.75,0.0,0.0);
+		glScalef(10.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.1,1.0,0.066);
+
+		glTranslatef(17.75,0.0,0.0);
+		glTranslatef(17.75,0.0,0.0);
+
+		glColor3f(1.0,1.0,1.0);
+		glScalef(10,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.1,1.0,0.066);
+
 		glColor3f(0.0,1.0,0.0);
 
 
@@ -140,10 +269,26 @@ void desenha_asa(float asax){
 		glScalef(27.0f,1.0,15.0f);
 		glutSolidCube(1.0f);
 		glScalef(0.037,1.0,0.066);
+
+
+		glTranslatef(-50,0.0,0.0);
+
+		glTranslatef(0.0,0.0,-7.5);
+		glScalef(13.5,0.1,0.1f);
+		glutSolidSphere(1, 10 ,10);
+		glScalef(0.074,10.0,10.0);
+
+		glTranslatef(0.0,0.0,7.5);
+		glColor3f(1.0,0.0,0.0);
+		glScalef(27.0f,1.0,15.0f);
+		glutSolidCube(1.0f);
+		glScalef(0.037,1.0,0.066);
+
 		glRotatef(-asax, 1.0, 0.0, 0.0);
 		
-		glTranslatef(-17.75,0.0,0.0);
-		glTranslatef(17.75,0.0,0.0);
+		
+
+		glRotatef(-180,0.0,1.0,0.0);
 
 	glPopMatrix();
 
